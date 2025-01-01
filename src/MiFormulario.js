b0894b2bf301cc58af33ed6f78c4c1e840fdfa3f
Emiliano <emilianoagu@hotmail.com>
2025-01-01 16:41:31 -0300

    Correccion submit

--- src/MiFormulario.js
+++ src/MiFormulario.js
@@ -1,28 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function MiFormulario() {
-  const [selectedOption, setSelectedOption] = useState('');
-  const [inputValue1, setInputValue1] = useState('');
-  const [inputValue2, setInputValue2] = useState('');
-  const [resultado, setResultado] = useState('');
+  const [selectedOption, setSelectedOption] = useState("");
+  const [inputValue1, setInputValue1] = useState("");
+  const [inputValue2, setInputValue2] = useState("");
+  const [resultado, setResultado] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    // Aquí podrías realizar los cálculos necesarios con los valores del formulario
-    // Por ahora, simplemente mostraremos un mensaje
-    setResultado({setSelectedOption, setInputValue1, setInputValue2});
+
+    // Creamos un array con los valores y sus tipos
+    /* const resultadoArray = [selectedOption, inputValue1, inputValue2]; */
 
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <select value={selectedOption} onChange={(e) => setSelectedOption(e.target.value)}>
+      <select
+        value={selectedOption}
+        onChange={(e) => setSelectedOption(e.target.value)}
+      >
         <option value="">Selecciona una opción</option>
         <option value="suma">Suma</option>
         <option value="resta">Resta</option>
       </select>
-      <input type="text" value={inputValue1} onChange={(e) => setInputValue1(e.target.value)} />
-      <input type="text" value={inputValue2} onChange={(e) => setInputValue2(e.target.value)} />
+      <input
+        type="text"
+        value={inputValue1}
+        onChange={(e) => setInputValue1(e.target.value)}
+      />
+      <input
+        type="text"
+        value={inputValue2}
+        onChange={(e) => setInputValue2(e.target.value)}
+      />
       <button type="submit">Calcular</button>
       {resultado && <p>{resultado}</p>}
     </form>
